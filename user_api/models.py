ba8baf61c83627b0ae0b9e3b1d4152bad65e97d3
Maria Boudeheb <135654272+maria-bd@users.noreply.github.com>
2024-04-18 03:39:59 +0100

    Update models.py

--- user_api/models.py
+++ user_api/models.py
@@ -1,35 +1,74 @@
 from django.db import models
-from django.contrib.auth.base_user import BaseUserManager
-from django.contrib.auth.models import AbstractBaseUser, PermissionsMixin
-
-class AppUserManager(BaseUserManager):
-	def create_user(self, email, password=None):
-		if not email:
-			raise ValueError('An email is required.')
-		if not password:
-			raise ValueError('A password is required.')
-		email = self.normalize_email(email)
-		user = self.model(email=email)
-		user.set_password(password)
-		user.save()
-		return user
-	def create_superuser(self, email, password=None):
-		if not email:
-			raise ValueError('An email is required.')
-		if not password:
-			raise ValueError('A password is required.')
-		user = self.create_user(email, password)
-		user.is_superuser = True
-		user.save()
-		return user
+from django.contrib.auth.models import AbstractUser
 
 
-class AppUser(AbstractBaseUser, PermissionsMixin):
-	user_id = models.AutoField(primary_key=True)
-	email = models.EmailField(max_length=50, unique=True)
-	username = models.CharField(max_length=50)
-	USERNAME_FIELD = 'email'
-	REQUIRED_FIELDS = ['username']
-	objects = AppUserManager()
-	def __str__(self):
-		return self.username
+class AppUser(AbstractUser):
+    # Add any additional fields or customization here
+    pass
+
+class Domain(models.Model):
+    domain_id = models.AutoField(primary_key=True)
+    domain_name = models.CharField(max_length=30)
+    domain_description = models.CharField(max_length=255)
+
+class Users(models.Model):
+    user_id = models.AutoField(primary_key=True)
+    name = models.CharField(max_length=255)
+    fam_name = models.CharField(max_length=30)
+    gender = models.CharField(max_length=30)
+    email = models.EmailField(unique=True, max_length=50)
+    photo = models.CharField(max_length=50)
+
+class Instructor(models.Model):
+    user = models.OneToOneField(Users, on_delete=models.CASCADE, primary_key=True)
+    user_type = models.IntegerField()
+    grade = models.CharField(max_length=255)
+
+class Learner(models.Model):
+    user = models.OneToOneField(Users, on_delete=models.CASCADE, primary_key=True)
+    type_user = models.IntegerField()
+    total_XP = models.IntegerField()
+
+class Account(models.Model):
+    user = models.OneToOneField(Users, on_delete=models.CASCADE, primary_key=True)
+    user_name = models.CharField(max_length=30)
+    user_password = models.CharField(max_length=255)
+
+class Training(models.Model):
+    training_id = models.AutoField(primary_key=True)
+    domain = models.ForeignKey(Domain, on_delete=models.CASCADE)
+    training_name = models.CharField(max_length=255)
+    training_description = models.CharField(max_length=500)
+
+class Lesson(models.Model):
+    lesson_id = models.AutoField(primary_key=True)
+    instructor = models.ForeignKey(Instructor, on_delete=models.CASCADE)
+    training = models.ForeignKey(Training, on_delete=models.CASCADE)
+    lesson_description = models.CharField(max_length=500)
+
+class IsEnrolled(models.Model):
+    lesson = models.ForeignKey(Lesson, on_delete=models.CASCADE)
+    user = models.ForeignKey(Learner, on_delete=models.CASCADE)
+    name = models.CharField(max_length=255)
+    class Meta:
+        unique_together = ('lesson', 'user',)
+
+class Video(models.Model):
+    id_vid = models.AutoField(primary_key=True)
+    lesson = models.ForeignKey(Lesson, on_delete=models.CASCADE)
+    XP_pts = models.IntegerField()
+    link_vid = models.CharField(max_length=255)
+
+class Tasks(models.Model):
+    id_task = models.AutoField(primary_key=True)
+    lesson = models.ForeignKey(Lesson, on_delete=models.CASCADE)
+    XP_pts = models.IntegerField()
+    question = models.CharField(max_length=300)
+    response = models.CharField(max_length=800)
+
+class Test(models.Model):
+    test_id = models.AutoField(primary_key=True)
+    lesson = models.ForeignKey(Lesson, on_delete=models.CASCADE)
+    question = models.CharField(max_length=300)
+    response = models.CharField(max_length=800)
+    mark = models.FloatField()
