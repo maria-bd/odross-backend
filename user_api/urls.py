7dc533014d64a00cbbb2acef0b62a8a2b8f35870
Maria Boudeheb <135654272+maria-bd@users.noreply.github.com>
2024-04-18 03:40:55 +0100

    Update urls.py

--- user_api/urls.py
+++ user_api/urls.py
@@ -1,9 +1,7 @@
 from django.urls import path
-from . import views
+from .views import user_list
 
 urlpatterns = [
-	path('register', views.UserRegister.as_view(), name='register'),
-	path('login', views.UserLogin.as_view(), name='login'),
-	path('logout', views.UserLogout.as_view(), name='logout'),
-	path('user', views.UserView.as_view(), name='user'),
+    path('users/', user_list, name='user_list'),
+    # Other URL patterns...
 ]
