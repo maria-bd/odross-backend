df7540959ec8bb067f32c4f98ba91c5f66568582
Maria Boudeheb <135654272+maria-bd@users.noreply.github.com>
2024-04-30 18:55:35 +0100

    Update views.py

--- user_api/views.py
+++ user_api/views.py
@@ -1,11 +1,29 @@
-from django.http import JsonResponse
-from .models import Users
-from .serializers import UsersSerializer
+from rest_framework import status
+from rest_framework.response import Response
+from rest_framework.views import APIView
+from rest_framework.permissions import AllowAny  # Import the permission class you want to use
+from .serializers import UserRegistrationSerializer, UserLoginSerializer
 
-def user_list(request):
-    if request.method == 'GET':
-        users = Users.objects.all()
-        serializer = UsersSerializer(users, many=True)
-        return JsonResponse(serializer.data, safe=False)
-    else:
-        return JsonResponse({"error": "Method not allowed"}, status=405)
+class UserRegistration(APIView):
+    permission_classes = [AllowAny]  # Set the permission class for this view
+    def post(self, request):
+        if request.method == 'POST':  # Check if the request method is POST
+            data = request.data
+            serializer = UserRegistrationSerializer(data=data)
+            if serializer.is_valid():
+                serializer.save()
+                return Response(serializer.data, status=status.HTTP_201_CREATED)
+            return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+        else:
+            return Response({"detail": "Method not allowed."}, status=status.HTTP_405_METHOD_NOT_ALLOWED)
+
+class UserLogin(APIView):
+    permission_classes = [AllowAny]
+
+    def post(self, request):
+        serializer = UserLoginSerializer(data=request.data)
+        if serializer.is_valid():
+            user = serializer.validated_data
+            # You can customize the response as per your requirement
+            return Response({'message': 'Login successful', 'name': user.name}, status=status.HTTP_200_OK)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
